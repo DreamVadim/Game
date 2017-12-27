7cfdc76343e2553700000c8edf37d354bdb80240
Vadim Mihailov <picturax@icloud.com>
2017-12-27 20:10:28 +0300

    Закоментировали код генерации врагов в Game.cpp

--- game/game/Game.cpp
+++ game/game/Game.cpp
@@ -25,21 +25,21 @@ void Game::randomFoodGenerate(std::string TileMap[], int width, int height) {//р
 	}
 }
 
-void Game::randomEnemyGenerate(std::string TileMap[], int width, int height) {//рандомно расставл¤ем врагов
-
-	int randomElementX = 0;//переменна¤ дл¤ хранени¤ случайного элемента по горизонтали
-	int randomElementY = 0;//переменна¤ дл¤ хранени¤ случайного элемента по вертикали
-
-	int countEnemy = 5;//количество камней
-
-	while (countEnemy > 0) {
-		randomElementX = 1 + rand() % (width - 1);//псевдослучайное значение по УxФ от 1 до
-												  //ширина карты-1. ќграничение введено чтобы не получать числа бордюра карты
-		randomElementY = 1 + rand() % (height - 1);//по УyФ
-
-		if (TileMap[randomElementY][randomElementX] == ' ') {//если встретили символ пробел, 
-			TileMap[randomElementY][randomElementX] = 's';  //то ставим туда камень.
-			countEnemy--;
-		}
-	}
-}
+//void Game::randomEnemyGenerate(std::string TileMap[], int width, int height) {//рандомно расставл¤ем врагов
+//
+//	int randomElementX = 0;//переменна¤ дл¤ хранени¤ случайного элемента по горизонтали
+//	int randomElementY = 0;//переменна¤ дл¤ хранени¤ случайного элемента по вертикали
+//
+//	int countEnemy = 5;//количество камней
+//
+//	while (countEnemy > 0) {
+//		randomElementX = 1 + rand() % (width - 1);//псевдослучайное значение по УxФ от 1 до
+//												  //ширина карты-1. ќграничение введено чтобы не получать числа бордюра карты
+//		randomElementY = 1 + rand() % (height - 1);//по УyФ
+//
+//		if (TileMap[randomElementY][randomElementX] == ' ') {//если встретили символ пробел, 
+//			TileMap[randomElementY][randomElementX] = 's';  //то ставим туда камень.
+//			countEnemy--;
+//		}
+//	}
+//}
